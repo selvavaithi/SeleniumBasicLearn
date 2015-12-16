5bbb1e7b06e8310e434909d526f05c37f21bcb58
selvavaithi <selvavaithi@gmail.com>
2015-12-16 16:14:00 +0530

    Small update

---
+++ SeleniumBasic/src/main/java/testGroupId/SeleniumBasic/TestAppMusic.java
@@ -0,0 +1,10 @@
+package testGroupId.SeleniumBasic;
+
+public class TestAppMusic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Testing");
+	}
+
+}
